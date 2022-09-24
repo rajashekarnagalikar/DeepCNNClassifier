d085f4886ade78c115aafa9cb9ea88d0b4a56695
rajashekarnagalikar <rajashekarnagalikar@gmail.com>
2022-09-24 19:00:30 +0530

    Prepare callbacks has been configured

--- src/deepClassifier/config/configuration.py
+++ src/deepClassifier/config/configuration.py
@@ -2,7 +2,8 @@ from deepClassifier.constants import CONFIG_FILE_PATH, PARAMS_FILE_PATH
 from deepClassifier.utils import read_yaml, create_directories
 from deepClassifier.entity import (
     DataIngestionConfig, 
-    PrepareBaseModelConfig
+    PrepareBaseModelConfig,
+    PrepareCallbacksConfig
 )
 from pathlib import Path
 import os
@@ -47,3 +48,19 @@ class ConfigurationManager:
         )
 
         return prepare_base_model_config
+
+def get_prepare_callback_config(self) -> PrepareCallbacksConfig:
+        config = self.config.prepare_callbacks
+        model_ckpt_dir = os.path.dirname(config.checkpoint_model_filepath)
+        create_directories([
+            Path(model_ckpt_dir),
+            Path(config.tensorboard_root_log_dir)
+        ])
+
+        prepare_callback_config = PrepareCallbacksConfig(
+            root_dir=Path(config.root_dir),
+            tensorboard_root_log_dir=Path(config.tensorboard_root_log_dir),
+            checkpoint_model_filepath=Path(config.checkpoint_model_filepath)
+        )
+
+        return prepare_callback_config
