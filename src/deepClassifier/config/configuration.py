cf1de2178f4ca88e3ae45a46b0b405e3df8781cd
rajashekarnagalikar <rajashekarnagalikar@gmail.com>
2022-09-29 19:38:36 +0530

    Stage 5 ie evaluation of model has been added

--- src/deepClassifier/config/configuration.py
+++ src/deepClassifier/config/configuration.py
@@ -2,9 +2,10 @@ from deepClassifier.constants import CONFIG_FILE_PATH, PARAMS_FILE_PATH
 from deepClassifier.utils import read_yaml, create_directories
 from deepClassifier.entity import (
     DataIngestionConfig, 
-    PrepareBaseModelConfig,
+    PrepareBaseModelConfig, 
     PrepareCallbacksConfig,
-    TrainingConfig
+    TrainingConfig,
+    EvaluationConfig
 )
 from pathlib import Path
 import os
@@ -17,19 +18,19 @@ class ConfigurationManager:
         self.config = read_yaml(config_filepath)
         self.params = read_yaml(params_filepath)
         create_directories([self.config.artifacts_root])
-        
+
     def get_data_ingestion_config(self) -> DataIngestionConfig:
         config = self.config.data_ingestion
-
+        
         create_directories([config.root_dir])
 
-        data_ingestion_config  = DataIngestionConfig(
+        data_ingestion_config = DataIngestionConfig(
             root_dir=config.root_dir,
             source_URL=config.source_URL,
             local_data_file=config.local_data_file,
-            unzip_dir=config.unzip_dir
+            unzip_dir=config.unzip_dir 
         )
-    
+
         return data_ingestion_config
 
     def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
@@ -86,4 +87,13 @@ class ConfigurationManager:
             params_image_size=params.IMAGE_SIZE
         )
 
-        return training_config    
+        return training_config
+
+    def get_validation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model=self.config.training.trained_model_path,
+            training_data=self.config.data_ingestion.unzip_dir,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
